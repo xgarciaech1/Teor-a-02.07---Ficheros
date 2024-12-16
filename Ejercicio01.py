d93ebe2e2cfb9272a75f4789f913ff2718e7729e
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-12-16 11:55:05 +0100

    Update Ejercicio01.py

--- Ejercicio01.py
+++ Ejercicio01.py
@@ -1,9 +1,11 @@
 #Escribir una función que pida un número entero entre 1 y 10 y guarde en un 
 #fichero con el nombre tabla-n.txt la tabla de multiplicar de ese número, 
 #donde n es el número introducido.
-n = int(input('Dime un numero entre el 1 y el 10: '))
-fichero = 'tabla' + str(n) + '.txt'
-f = open(fichero, 'w')
-for i in range(1, 11):
-    f.write(str(n) + 'x' + str(i) + '=' + str(n * i) + '\n')
-f.close
+def multiplicar():
+    n = int(input('Dime un numero entre el 1 y el 10: '))
+    fichero = 'tabla' + str(n) + '.txt'
+    f = open(fichero, 'w')
+    for i in range(1, 11):
+        f.write(str(n) + 'x' + str(i) + '=' + str(n * i) + '\n')
+    f.close
+multiplicar()
