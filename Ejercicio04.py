d65d37b96e84eecff2f0f22e76c8abc29861fb2c
xgarciaech1 <xgarciaech@educacion.navarra.es>
2024-12-30 12:24:45 +0100

    Update Ejercicio04.py

--- Ejercicio04.py
+++ Ejercicio04.py
@@ -1,6 +1,6 @@
 #Escribir un programa que acceda al fichero de internet mediante la url
 #indicada y muestre por pantalla el número de palabras que contiene.
-import urllib.request
+'''import urllib.request
 
 
 def read_url(url):
@@ -13,4 +13,25 @@ def read_url(url):
 
 url = 'http://textfiles.com/adventure/aencounter.txt'
 read_url(url)
+'''
 
+def contar_palabras(url):
+    '''
+    Función que recibe recibe la url de un fichero de texto y devuelve el número de palabras que contiene.
+    Parámetros:
+        url: Es una cadena con la url del fichero de texto.
+    Devuelve:
+        El número de palabras que contiene el fichero de texto daro por la url.
+    '''
+    from urllib import request
+    from urllib.error import URLError
+    try:
+        f = request.urlopen(url)
+    except URLError:
+        return('¡La url ' + url + ' no existe!')
+    else:
+        contenido = f.read()
+        return len(contenido.split())
+
+print(contar_palabras('https://www.gutenberg.org/files/2000/2000-0.txt'))
+print(contar_palabras('https://no-existe.txt'))
